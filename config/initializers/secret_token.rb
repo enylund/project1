8eab89f697ea25c2060c8b86661b99e07a86e278
djerek <derekgritz@yahoo.com>
2013-06-14 16:55:58 -0400

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BiblioApp::Application.config.secret_key_base = '4c96310645b12e12deba718ef3bff5eb3b8f373509266970e648c527cb7db321cc583eb2a9329112ccadb03a9196f2e793b0e00433e16c7e1c33985b2b6fed86'
