b4ab173ebd8cb8c9dcc4ca83f59be6259126eacf
enylund <eric.nylund@yale.edu>
2013-06-14 16:50:10 -0400

    removed file with wrong version of rails

--- demo_rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoRailsApp::Application.config.secret_token = '1b48ad4e3f8047fbb3770017b2f2951d8c8fa15c974c03081cde2ee52683ffed4173868251bde78973edf0df970652e553a36394d8d15b8eb601558a3215236d'
