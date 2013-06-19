1b57c0c44cab6f0666ca1dfdda60a184e3d7ddd8
djerek <derekgritz@yahoo.com>
2013-06-19 13:42:25 -0400

    made posts affiliate with the posting user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
   def current_user
   	if session[:user_id]
-  	  @current_user ||= User.find(session[:user_id]).username
+  	  @current_user ||= User.find(session[:user_id])
   	else
   	  @current_user = nil
   	end
