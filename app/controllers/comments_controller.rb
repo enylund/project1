d9c91e44b11a62d7da2905ad547b617b6e59f3df
djerek <derekgritz@yahoo.com>
2013-06-19 14:14:42 -0400

    comments capture the user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,5 +19,5 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
