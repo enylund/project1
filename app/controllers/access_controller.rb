d20a9cf7ada3bc496017bad5222e34d41b769091
enylund <eric.nylund@yale.edu>
2013-06-19 12:18:52 -0400

    added logout functionality

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -12,10 +12,15 @@ class AccessController < ApplicationController
       @the_user = User.find_by_username(params[:user][:username])
       session[:user_id] = @the_user.id
       flash[:notice] = "You have successfully logged in"
-      redirect_to user_path(1)
+      redirect_to user_path(session[:user_id])
     else
       flash[:notice] = "Invalid username or password"
       redirect_to :back
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 BiblioApp::Application.routes.draw do
   get "access/menu"
-  get "access/login"
+  get "access/login", :as => 'login'
+  get "logout" => 'access#destroy', :as => 'logout'
   post "access/attempt_login"
   match "login" => 'access#login', :via => :get
-  
+
   resources :channels
 
   resources :comments
@@ -14,7 +15,7 @@ BiblioApp::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'users#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
