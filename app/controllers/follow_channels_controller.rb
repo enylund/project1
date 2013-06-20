20cb32bc27f08ca23c1ba717138fac5b2cf88119
enylund <eric.nylund@yale.edu>
2013-06-20 15:20:04 -0400

    Added functionality to both follow and unfollow

--- app/controllers/follow_channels_controller.rb
+++ app/controllers/follow_channels_controller.rb
@@ -6,8 +6,11 @@ class FollowChannelsController < ApplicationController
   end
 
   def destroy
-    @follow = FollowChannel.find(params[:user_id])
-    @follow.destroy
+    @current_user_id_to_destroy_follow = params[:follow_channel][:user_id].to_i
+    @current_channel_id_to_destroy_follow = params[:follow_channel][:channel_id].to_i
+
+    @follow_id_to_destroy = FollowChannel.destroy_all(:channel_id => @current_channel_id_to_destroy_follow, :user_id => @current_user_id_to_destroy_follow).first.id
+    redirect_to :back
   end
 
   private
