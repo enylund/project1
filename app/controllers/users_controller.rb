57dd76cb556722a629ee3a164eb6ea78eaf6a9d4
enylund <eric.nylund@yale.edu>
2013-06-23 11:33:30 -0400

    Add a privacy check to hide private channels on a users show page when other users visit it. Also refactored some code in the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
+
+    private_channel_show_check()
+    create_array_of_followed_channels_to_show()
+
+  end
+
+  def private_channel_show_check
+    if current_user != @user.id
+      @channels_to_show = @user.channels.where('privacy != ?', 'private').all
+      # raise @channels_to_show.to_yaml
+    end
+  end
+
+  def create_array_of_followed_channels_to_show
     user_follows = FollowChannel.find_all_by_user_id(params[:id])
     the_channels = Array.new
     user_follows.each do |row|
@@ -8,4 +23,6 @@ class UsersController < ApplicationController
     end
     @array_of_channels_a_user_follows = Channel.find(the_channels)
   end
+
+
 end
