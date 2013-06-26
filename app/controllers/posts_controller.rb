f175687a9eed1026b777cd28c8a0afd257e98552
djerek <derekgritz@yahoo.com>
2013-06-26 09:38:29 -0400

    fix making posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :post_privacy
+  before_action :post_privacy, :except => [:create]
 
   # GET /posts
   # GET /posts.json
