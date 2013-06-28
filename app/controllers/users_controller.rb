dc85e7646799b79d5976c7822ada0912fb56aad9
djerek <derekgritz@yahoo.com>
2013-06-28 10:07:39 -0400

    user pages show post images, fixed deprecated methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_username(params[:username])
+    # @user = User.where(username: params[:username]).take
     @title = @user.username + "'s biblio"
     private_channel_show_check()
     create_array_of_followed_channels_to_show()
@@ -10,15 +11,15 @@ class UsersController < ApplicationController
 
   def private_channel_show_check
     if current_user != @user
-      @channels_to_show = @user.channels.where('privacy != ?', 'private').all
+      @channels_to_show = @user.channels.where('privacy != ?', 'private')
       # raise @channels_to_show.to_yaml
     else
-      @channels_to_show = @user.channels.all
+      @channels_to_show = @user.channels
     end
   end
 
   def create_array_of_followed_channels_to_show
-    user_follows = FollowChannel.find_all_by_user_id(@user.id)
+    user_follows = FollowChannel.where(user_id: @user.id)
     the_channels = Array.new
     user_follows.each do |row|
       the_channels << row[:channel_id]
