ba19992ff4b426ff0d97ec7e827625a3a2c16172
djerek <derekgritz@yahoo.com>
2013-07-02 10:59:01 -0400

    can delete collaborators and cant add a current collaborator

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,6 +1,19 @@
 class CollaborationsController < ApplicationController
   def create
-    Collaboration.create(collaboration_params)
+    the_chan = collaboration_params[:channel_id]
+    the_user = collaboration_params[:user_id]
+
+    if Collaboration.where(channel_id: the_chan, user_id: the_user).blank?
+      Collaboration.create(collaboration_params)
+      redirect_to :back
+    else
+      flash[:notice] = "This person is already a collaborator."
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    Collaboration.destroy_all( id: params[:id] )
     redirect_to :back
   end
 
