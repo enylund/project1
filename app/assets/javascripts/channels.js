325f3a6bc7232e6362d5274b3047f1e82db05d54
enylund <eric.nylund@yale.edu>
2013-07-05 10:03:35 -0400

    circular highlighting to remaining hovers.

--- app/assets/javascripts/channels.js
+++ app/assets/javascripts/channels.js
@@ -3,9 +3,9 @@ $(document).ready( function () {
 $('#expand-symbol').on('click', function() {
 
     if ($('#channel-info-hide').is(":hidden")) {
-        $("#expand-symbol").empty().html(" &#11033;");
+        $("#expand-symbol").empty().html("&#11033;");
       } else {
-        $("#expand-symbol").empty().html(" &#11031;");
+        $("#expand-symbol").empty().html("&#11031;");
       }
 
     $('#channel-info-hide').slideToggle();
